6f5e89f093beca46b4d4924d2c9cba7e44ed672f
Protiana <protiana@yandex.ru>
2022-08-15 13:48:11 +0300

    first task

---
+++ Examples/home1/Program.cs
@@ -0,0 +1,19 @@
+﻿//  найти меньшее из двух вводимых чисел
+
+Console.WriteLine("enter numberA");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter numberB");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int min = 0;
+
+if (numberA > numberB){
+    min = numberB;
+}
+else{
+    min = numberA;
+}
+
+Console.WriteLine(min);
+
