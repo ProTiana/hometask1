5af3be0135fa35c18809fafdf127b685abf14164
Protiana <protiana@yandex.ru>
2022-08-15 21:19:29 +0300

    task1 uprrade

--- Examples/home1/Program.cs
+++ Examples/home1/Program.cs
@@ -5,15 +5,19 @@ int numberA = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("enter numberB");
 int numberB = Convert.ToInt32(Console.ReadLine());
-
+int max = 0;
 int min = 0;
 
 if (numberA > numberB){
+    max = numberA;
     min = numberB;
 }
 else{
     min = numberA;
+    max = numberB;
 }
-
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
 Console.WriteLine(min);
 
