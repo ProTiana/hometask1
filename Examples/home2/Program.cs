855cd354eff0fa390085bb029e55d53f10943357
Protiana <protiana@yandex.ru>
2022-08-17 13:55:22 +0300

    task 2

---
+++ Examples/home2/Program.cs
@@ -0,0 +1,25 @@
+﻿//  найти максимальное из 3х чисел
+
+Console.WriteLine("enter numberA");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter numberB");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter numberС");
+int numberС = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if (numberA > numberB){
+    max = numberA;
+}
+else{
+    if (numberB>numberС){
+        max = numberB;}
+    else{
+        max = numberС;}}
+  
+Console.Write("max = ");
+ Console.WriteLine(max);
+
