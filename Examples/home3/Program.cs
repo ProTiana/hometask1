a31a68d9781af38c63de899b0ad1d830aed86ea8
Protiana <protiana@yandex.ru>
2022-08-17 19:57:44 +0300

    3d task

---
+++ Examples/home3/Program.cs
@@ -0,0 +1,13 @@
+﻿// определить, является ли вводимое число четным (деление на 2 без остатка ==0)
+
+  Console.WriteLine("Введите число");
+  int number1 = Convert.ToInt32(Console.ReadLine());
+  if ((number1%2) == 0){
+    Console.WriteLine("Четное");
+  }
+ else{
+Console.WriteLine("Нечетное");
+ }
+
+
+
