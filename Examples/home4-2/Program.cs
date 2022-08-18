6602bdbeff12f250629a882822dea8f11cd877ec
Protiana <protiana@yandex.ru>
2022-08-18 22:00:45 +0300

    task4

---
+++ Examples/home4-2/Program.cs
@@ -0,0 +1,11 @@
+﻿// принимает на вход число  n и возвращает все четные числа от 1 до n
+Console.WriteLine("add number");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (a<n){
+    if ((a % 2) == 0){
+    Console.Write(a);
+    }
+ a++;
+}
+
