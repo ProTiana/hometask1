ba7fb3edb8e76e641efd8bae5d725975070e0ae8
Protiana <protiana@yandex.ru>
2022-09-08 13:54:00 +0300

    task 4-3

---
+++ Home_4/task4-3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задать массив из 8 элементов и выводит их не экран
+
+int[] HW = new int[8];
+
+int[] Fillarray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 1000);
+    }
+    return array;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+
+    }
+}
+
+ int[]Result = Fillarray(HW);
+ PrintArray(Result);
+
+
