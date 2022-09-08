8b0138464fe9716c2624acc4093b5118f3aa6089
Protiana <protiana@yandex.ru>
2022-09-08 13:53:23 +0300

    task 4-2

---
+++ Home_4/task4-2/Program.cs
@@ -0,0 +1,24 @@
+﻿// ввести число и посчитать сумму его цифр
+
+Console.WriteLine("Введите число");
+string A = Convert.ToString(Console.ReadLine());
+
+int FindSum(string a){
+    int result = 0;
+for(int i= 0; i< a.Length; i++){ 
+    string s1 = char.ToString(a[i]); // конвертируем символ строки пользователя в отдельную строку
+int tmp = Int32.Parse(s1); // конвертируем новую строку a[i] в число int
+    result = result + tmp;
+}
+    return result;
+}
+int sum = FindSum(A);
+Console.WriteLine(sum);
+
+
+
+
+/*for(int i= 0; i< A.Length; i++){
+int s = Convert.ToInt32(A[i]);
+Console.WriteLine(s);}*/
+
