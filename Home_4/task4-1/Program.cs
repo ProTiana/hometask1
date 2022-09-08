2ad12a52fd21f335779fb9af612871a78f56f62c
Protiana <protiana@yandex.ru>
2022-09-08 13:51:28 +0300

    task 4-1

---
+++ Home_4/task4-1/Program.cs
@@ -0,0 +1,29 @@
+﻿// принять на входь 2 числа (a и b) и выдать число а, возведенное в натуральную степеь  b
+
+Console.WriteLine("Введите число");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите  нужную степень");
+double b = Convert.ToDouble(Console.ReadLine());
+
+
+double FindPower(double x, double y)
+{
+
+    double result = 0;
+    y = Math.Round(y);
+
+    if (y > 0)
+    {
+        result = Math.Pow(x, y);
+    }
+    else
+    {
+        y = y * -1;
+        result = Math.Pow(x, y);
+    }
+    return result;
+}
+
+double power = FindPower(a, b);
+Console.WriteLine(power);
