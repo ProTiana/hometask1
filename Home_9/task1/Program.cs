1a0f3fc767ae66a21e11c0c44763f5fece050914
Protiana <protiana@yandex.ru>
2022-09-23 00:59:38 +0300

    hometask9

---
+++ Home_9/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Принять на вход 2 чимла и вывести все натуральные числа в отрезке между заданными.
+
+Console.WriteLine("Введите число начала");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число конца");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbers(int min, int max)
+{
+   
+if(min < max){
+    for (int i = min; i <= max; i++)
+    Console.Write($"{i} ; ");
+}
+else{
+    Console.WriteLine("Ошибка: Число начала меньше числа конца");
+}
+    }
+    
+PrintNumbers(m, n);
---
+++ Home_9/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Принять на вход 2 чимла и посчитать сумму всех натуральных чисел в отрезке между ними
+
+Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int FindSumBetween(int a, int b){
+    int sum = 0;
+if(a < b){
+    for(int i = a; i<= b; i++){
+        sum += i;
+        }
+    }
+else{
+    Console.WriteLine("Ошибка: Число начала меньше числа конца");
+    }
+    return sum;
+}
+
+int Result = FindSumBetween(m, n);
+Console.WriteLine(Result);
+
